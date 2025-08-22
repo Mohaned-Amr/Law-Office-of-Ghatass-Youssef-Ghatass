15c58fd1c958321fd8b59ac823eaa8d5a58b751c
Mohaned <mohaned.repilt44@gmail.com>
2025-08-22 20:11:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// ينزل لجزء التواصل
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+}
+// Placeholder for future interactivity
+console.log("موقع مكتب المحامي غطاس يوسف غطاس جاهز للعمل.");
